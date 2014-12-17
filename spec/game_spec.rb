49c5efc7565e674ae4a65de483b4a726a49ea5fb
Clint <clintp1@gmail.com>
2014-12-17 08:40:43 +0000

    added possible test for the game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,26 @@
 require 'game'
 
 describe Game do
+
+  it 'can initialize a new game with two players' do
+
+  end
+
+  it 'should allow a player to place ships on his board' do
+
+  end
+
+
+  it 'shuld allow a player to look on his opponents board to see where he shot at without showing opponets ship' do
+
+  end
+
+  it 'should allow a player to shoot at opponents board' do
+
+  end
+
+  it 'should report if a player has won' do
+
+  end
   
 end
