69680d8dd4d5bf3b43c469cad2b79f10d4f93fc2
Clint <clintp1@gmail.com>
2014-12-17 19:54:00 +0000

    refactored board specs again

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -25,29 +25,27 @@ describe Board do
   it 'should not be empty after a ship is added' do
     allow(ship).to receive(:coordinates).and_return([[0,0],[1,0],[2,0]])
     board.add_ship(ship)
-    expect(board).to_not be_empty
+    expect(board).not_to be_empty
   end
 
   it 'should be able to be shot at' do
     board.receive_shot(1,1) #B2
-    expect(board.show).to eq [[WATER,WATER,WATER],[WATER,MISS,WATER],[WATER,WATER,WATER]]
+    expect(board).not_to be_empty
   end
 
   it 'should not be able to take shots on negative coordinates' do
     board.receive_shot(1,-1) 
-    expect(board.show).to eq [[WATER,WATER,WATER],[WATER,WATER,WATER],[WATER,WATER,WATER]]
+    expect(board).to be_empty
   end
 
   it 'should not be able to take shots outside the boundaries' do
     board.receive_shot(1,40) 
-    expect(board.show).to eq [[WATER,WATER,WATER],[WATER,WATER,WATER],[WATER,WATER,WATER]]
+    expect(board).to be_empty
   end
 
   it 'should accept shots on the boundaries' do
     board.receive_shot(0,0) 
-    expect(board.show).to eq [[MISS,WATER,WATER],[WATER,WATER,WATER],[WATER,WATER,WATER]]
-    board.receive_shot(2,2) 
-    expect(board.show).to eq [[MISS,WATER,WATER],[WATER,WATER,WATER],[WATER,WATER,MISS]]
+    expect(board).not_to be_empty
   end
 
 
