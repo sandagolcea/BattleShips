021754171f6f03942038dcf29da998ac846360c5
Clint <clintp1@gmail.com>
2014-12-17 17:01:09 +0000

    try on player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,28 @@
 class Player
 
+
+
+
+  def initialize(name)
+    @name = name
+    @ships = []
+      
+    end
+
+
+    def self.place_ship(start_point, direction)
+      # if direction == :vertical
+      #   cell_nr = 0
+      #   cell_nr.upto(battleship.length)
+      #   point = start_point.last.to_ +cell_nr
+      #   ary << start_point.first + point.to_s
+      # else
+        # cell_nr = 0
+        # cell_nr.upto(battleship.length)
+
+
+      # end  
+    end
   # def place_ship(start_point,length,direction)
   # A1, direction = horizontal , length = 3 => 
   # ary ["A1"]
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,9 +2,14 @@ require 'player'
 
 describe Player do
 
-  # it 'can place ships on the board' do
+  let(:player){Player.new()}
+  let(:ship){double :ship, length => 3}
+
+
+  it 'can place ships on the board' do
+    # expect(player.place_ship("A1", :vertical)).to eq "A1A2A3A4"
   #   allow(board).to receive(:baby_ship).and_return(true)
-  # end
+  end
   
   it 'should be able to take a shot' do
   end
